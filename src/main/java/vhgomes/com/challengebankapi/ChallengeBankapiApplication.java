42fcaa463ee3a172fab42a4db0e826234068a420
Victor Hugo <vhclaudiogomes@gmail.com>
2024-06-30 15:35:13 -0300

    initial commit

---
+++ src/main/java/vhgomes/com/challengebankapi/ChallengeBankapiApplication.java
@@ -0,0 +1,13 @@
+package vhgomes.com.challengebankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBankapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeBankapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vhgomes/com/challengebankapi/ChallengeBankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package vhgomes.com.challengebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBankapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
