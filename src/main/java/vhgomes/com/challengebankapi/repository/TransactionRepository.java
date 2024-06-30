22f69622a10ba1005128a279b936d5b552b074ab
Victor Hugo <vhclaudiogomes@gmail.com>
2024-06-30 20:32:24 -0300

    Transaction/Repository | changed the id to string on repository

--- src/main/java/vhgomes/com/challengebankapi/repository/TransactionRepository.java
+++ src/main/java/vhgomes/com/challengebankapi/repository/TransactionRepository.java
@@ -3,5 +3,5 @@ package vhgomes.com.challengebankapi.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import vhgomes.com.challengebankapi.models.Transaction;
 
-public interface TransactionRepository extends MongoRepository<Transaction, Long> {
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
 }
